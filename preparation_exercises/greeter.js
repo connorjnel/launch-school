123c5fd51629924b3c1e270c72e18e8c57933c57
Jovan Nel <neljovan88@gmail.com>
2022-08-04 08:41:11 +0100

    launch school exercises

---
+++ preparation_exercises/greeter.js
@@ -0,0 +1,9 @@
+let name = "Victor";
+
+function greet(name) {
+	console.log(`Good Morning, ${name}.`);
+	console.log(`Good Afternoon, ${name}.`);
+	console.log(`Good Evening, ${name}.`);
+}
+
+greet(name);
