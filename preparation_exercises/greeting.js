26774c180bc627222f3d3737bb2cde5ae354f74f
Jovan Nel <neljovan88@gmail.com>
2022-08-10 13:51:41 +0100

    ls basics

---
+++ preparation_exercises/greeting.js
@@ -0,0 +1,54 @@
+// let greetingMessage = "Good Morning!";
+
+// function greetPeople() {
+// 	console.log(greetingMessage);
+// }
+
+// function changeGreetingMessage(newMessage) {
+// 	greetingMessage = newMessage;
+// }
+
+// changeGreetingMessage("Good Evening");
+// greetPeople();
+
+// function greetPeople(greetingMessage) {
+// 	console.log(greetingMessage);
+// }
+
+// greetPeople("Good Morning!");
+
+// function changeFirstElement(array) {
+// 	array[0] = 9;
+// }
+
+// let oneToFive = [1, 2, 3, 4, 5];
+// changeFirstElement(oneToFive);
+// console.log(oneToFive);
+
+// function addToArray(array) {
+// 	return array.concat(10);
+// }
+
+// let oneToFive = [1, 2, 3, 4, 5];
+// console.log(addToArray(oneToFive));
+// console.log(oneToFive);
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+let sum = add(20, 45);
+console.log(sum);
+
+let difference = subtract(80, 10);
+console.log(difference);
+
+function times(num1, num2) {
+	return num1 * num2;
+}
+
+console.log(times(add(20, 45), subtract(80, 10)));
---
+++ preparation_exercises/say.js
@@ -0,0 +1,15 @@
+function say(words = "hello") {
+	console.log(words + "!");
+}
+
+say("Howdy");
+say();
+
+function add(a, b) {
+	return a + b;
+}
+
+add(2, 3);
+
+let twoAndThree = add(2, 3);
+console.log(twoAndThree);
