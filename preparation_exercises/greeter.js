bd6b35c25401d3fab22371cd6e50e24823fa8033
Jovan Nel <neljovan88@gmail.com>
2022-08-11 09:27:49 +0100

    ls js basics

--- preparation_exercises/greeter.js
+++ preparation_exercises/greeter.js
@@ -1,9 +1,39 @@
-let name = "Victor";
+// let name = "Victor";
 
-function greet(name) {
-	console.log(`Good Morning, ${name}.`);
-	console.log(`Good Afternoon, ${name}.`);
-	console.log(`Good Evening, ${name}.`);
+// function greet(name) {
+// 	console.log(`Good Morning, ${name}.`);
+// 	console.log(`Good Afternoon, ${name}.`);
+// 	console.log(`Good Evening, ${name}.`);
+// }
+
+// greet(name);
+
+// function greeter(first, lastName) {
+// 	first = prompt("What is your first name?");
+// 	lastName = prompt("What is your last name?");
+// 	alert(`Welcome and salutations ${first} ${lastName}`);
+// }
+
+// greeter();
+
+// function getName(prompt) {
+// 	let readlineSync = require("readline-sync");
+// 	let name = readlineSync.question(prompt);
+// 	return name;
+// }
+
+// let firstName = getName("What is your first name? ");
+// let lastName = getName("What is your last name? ");
+// console.log(`Hello, ${firstName} ${lastName}!`);
+
+function getnums(prompt) {
+	let readlineSync = require("readline-sync");
+	let nums = readlineSync.question(prompt);
+	return nums;
 }
 
-greet(name);
+const multiply = (num1, num2) => num1 * num2;
+
+let num1 = Number(getnums("Enter the first number: \n"));
+let num2 = Number(getnums("Enter the second number: \n"));
+console.log(multiply(num1, num2));
--- preparation_exercises/greeting.js
+++ preparation_exercises/greeting.js
@@ -33,22 +33,30 @@
 // console.log(addToArray(oneToFive));
 // console.log(oneToFive);
 
-function add(a, b) {
-	return a + b;
-}
+// function add(a, b) {
+// 	return a + b;
+// }
 
-function subtract(a, b) {
-	return a - b;
-}
+// function subtract(a, b) {
+// 	return a - b;
+// }
 
-let sum = add(20, 45);
-console.log(sum);
+// let sum = add(20, 45);
+// console.log(sum);
 
-let difference = subtract(80, 10);
-console.log(difference);
+// let difference = subtract(80, 10);
+// console.log(difference);
 
-function times(num1, num2) {
-	return num1 * num2;
-}
+// function times(num1, num2) {
+// 	return num1 * num2;
+// }
 
-console.log(times(add(20, 45), subtract(80, 10)));
+// console.log(times(add(20, 45), subtract(80, 10)));
+
+// greetPeople();
+
+// function greetPeople() {
+// 	console.log("Good Morning!");
+// }
+
+let add = (a, b) => a + b;
