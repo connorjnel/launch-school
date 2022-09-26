5f1cf03daf7d7a6b6ee9bc4b498122a4011a0e9f
connorjnel <connor.j.nel00@gmail.com>
2022-09-26 10:31:17 +0100

    loops

--- preparation_exercises/loop-exercises.js
+++ preparation_exercises/loop-exercises.js
@@ -10,3 +10,27 @@
 // 		break;
 // 	}
 // }
+
+// function randomNumberBetween(min, max) {
+// 	return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+
+// let tries = 0;
+// let result;
+
+// do {
+// 	result = randomNumberBetween(1, 6);
+// 	tries += 1;
+// } while (result <= 2);
+
+// console.log("It took " + String(tries) + " tries to get a number greater than 2");
+
+function factorial(number) {
+	if (number === 1) {
+		return 1;
+	}
+
+	return number * factorial(number - 1);
+}
+
+console.log(factorial(5)); // => 120
