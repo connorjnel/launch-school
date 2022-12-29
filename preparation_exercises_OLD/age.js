68210a1f7e4da2cabca00659a2cfde5e9ab135ef
connorjnel <connor.j.nel00@gmail.com>
2022-12-29 16:56:27 +0000

    ls

--- preparation_exercises_OLD/age.js
+++ preparation_exercises_OLD/age.js
@@ -9,6 +9,6 @@ console.log(`You are ${age} years old.`);
 // console.log(`In 30 years, you will be ${age + 30} years old.`);
 // console.log(`In 40 years, you will be ${age + 40} years old.`);
 
-for (let future = 10; future <= 40; future += 10) {
-	console.log(`In ${future} years, you will be ${age + future} years old.`);
-}
+// for (let future = 10; future <= 40; future += 10) {
+// 	console.log(`In ${future} years, you will be ${age + future} years old.`);
+// }
